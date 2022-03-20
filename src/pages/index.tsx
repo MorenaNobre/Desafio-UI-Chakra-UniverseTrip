a18ec277e48d5362e04c2d323da408f69013b982
MorenaNobre <morenagnobre@gmail.com>
2022-03-20 12:06:28 -0300

    Added Divider and Heading in main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 import Categories from "../components/Categories";
+import Divider from "../components/Divider";
 
 export default function Home() {
   return (
@@ -11,6 +12,18 @@ export default function Home() {
       <Header />
       <Banner />
       <Categories />
+      <Divider />
+
+      <Heading
+        textAlign="center"
+        fontWeight="500"
+        mb={["5", "14"]}
+        fontSize={["lg", "3xl", "4xl"]}
+      >
+        O Gerador de Improbabilidade infinita
+        <br />
+        te leva!
+      </Heading>
     </Flex>
-  )
+  );
 }
