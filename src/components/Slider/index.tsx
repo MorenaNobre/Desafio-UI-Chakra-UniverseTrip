df587b0dadc21bfe2f86f5f7c3864454139c522f
MorenaNobre <morenagnobre@gmail.com>
2022-03-31 15:40:08 -0300

    Applied Prismic to the Home page

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -2,10 +2,11 @@ import { Flex, Heading, Link, Text } from "@chakra-ui/react";
 
 import SwiperCore, { Navigation, Pagination, A11y } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { GalaxyProps } from "../../pages";
 
 SwiperCore.use([Navigation, Pagination, A11y])
 
-export default function Slider() {
+export default function Slider({ galaxys }: GalaxyProps) {
   return (
     <Flex
       w="100%"
@@ -23,33 +24,33 @@ export default function Slider() {
         }}
         style={{ width: "100%", flex: "1" }}
       >
-        <SwiperSlide>
+       {galaxys.map(galaxy => (
+          <SwiperSlide>
           <Flex
             w="100%"
             h="100%"
             align="center"
             justify="center"
             direction="column"
-            bgImage="url(/magrathea.png)"
+            bgImage={`url(${galaxy.image})`}
             bgRepeat="no-repeat"
             bgSize="cover"
             textAlign="center"
             bgPosition="center"
           >
-            <Link href="/destination/place">
+            <Link href={`/galaxy/${galaxy.slug}`}>
               <a>
                 <Heading
                   fontSize={["3xl", "4xl", "5xl"]}
                   fontWeight="bold"
                   bgColor="gray.800"
                   color="yellow.400"
-                  // p="1"
                   pt={["2", "4"]}
                   px="4"
                   borderTopLeftRadius="8"
                   borderTopRightRadius="8"
                 >
-                  Magrathea
+                  {galaxy.title}
                 </Heading>
                 <Text
                   fontWeight="bold"
@@ -63,58 +64,13 @@ export default function Slider() {
                   borderBottomRightRadius="8"
                   borderBottomLeftRadius="8"
                 >
-                  Indústria especializada em construção de planetas
+                  {galaxy.summary}
                 </Text>
               </a>
             </Link>
           </Flex>
         </SwiperSlide>
-        <SwiperSlide>
-          <Flex
-            w="100%"
-            h="100%"
-            align="center"
-            justify="center"
-            direction="column"
-            bgImage="url(/magrathea.png)"
-            bgRepeat="no-repeat"
-            bgSize="cover"
-            textAlign="center"
-            bgPosition="center"
-          >
-            <Link href="/destination/place">
-              <a>
-                <Heading
-                  fontSize={["3xl", "4xl", "5xl"]}
-                  fontWeight="bold"
-                  color="yellow.400"
-                  // p="1"
-                  bgColor="gray.800"
-                  pt={["2", "4"]}
-                  px="4"
-                  borderTopLeftRadius="8"
-                  borderTopRightRadius="8"
-                >
-                  Magrathea-1
-                </Heading>
-                <Text
-                  fontWeight="bold"
-                  fontSize={["0.8rem", "1xl", "2xl"]}
-                  bgColor="gray.800"
-                  color="yellow.400"
-                  // p="1"
-                  pt={["2", "4"]}
-                  pb={["2", "4"]}
-                  px="4"
-                  borderBottomRightRadius="8"
-                  borderBottomLeftRadius="8"
-                >
-                  Indústria especializada em construção de planetas
-                </Text>
-              </a>
-            </Link>
-          </Flex>
-        </SwiperSlide>
+       ))}
       </Swiper>
     </Flex>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Flex, Heading } from "@chakra-ui/react";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 import Categories from "../components/Categories";
 import Divider from "../components/Divider";
 import Slider from "../components/Slider";
+import { GetStaticProps } from "next";
+import { getPrismicClient } from "../services/prismic";
+import Prismic from "@prismicio/client";
 
-export default function Home() {
+export interface GalaxyProps {
+  galaxys: {
+    slug: string;
+    title: string;
+    summary: string;
+    image: string;
+  }[];
+}
+
+export default function Home({ galaxys }: GalaxyProps) {
   return (
     <Flex direction="column">
       <Header />
@@ -25,7 +35,31 @@ export default function Home() {
         <br />
         te leva!
       </Heading>
-      <Slider />
+      <Slider galaxys={galaxys}/>
     </Flex>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.Predicates.at("document.type", "galaxy"),
+  ]);
+
+  const galaxys = response.results.map((galaxy) => {
+    return {
+      slug: galaxy.uid,
+      title: galaxy.data.title,
+      summary: galaxy.data.summary,
+      image: galaxy.data.slider_image.url,
+    };
+  });
+
+  return {
+    props: {
+      galaxys,
+    },
+    revalidate: 1800,
+  };
+};
